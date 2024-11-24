3ccce829fc4eec857efb1f427dd76c690847540d
EngineerMoore <alexiashalise@gmail.com>
2024-11-24 14:51:26 -0600

    created auth route with registration and login middleware; corrected server, package-json, and .http files

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
+require("dotenv").config();
 const express = require(`express`);
 const app = express();
 
-PORT = 3000;
+const PORT = 3000;
 
 app.use(express.json());
 
 app.use(require(`morgan`)(`dev`));
 
+app.use(require(`./API/auth`).router)
+
 app.use((req, res, next) => {
   next({ status: 404, message: `Endpoint not found`});
 })
 
+app.use((e, req, res, next) => {
+  console.error(e);
+  res
+    .status( e.status ?? 500)
+    .json(e.message ?? `Something went wrong.`)
+})
 
 app.listen(PORT, () => {
   console.log(`You are now listening on port ${PORT}`);
